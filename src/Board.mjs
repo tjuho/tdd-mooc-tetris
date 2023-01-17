dff138822e5452a48d3dd5c914c2ca61145feb41
Juho Taipale <krooh@hotmail.com>
2023-01-17 04:47:21 +0200

    tetromino stops on top of other tetromino

--- src/Board.mjs
+++ src/Board.mjs
@@ -29,7 +29,7 @@ export class Board {
   }
   
   isEmpty(x,y){
-    return !(this.boardState[y][x] == ".");
+    return (this.boardState[y][x] == ".");
   }    
   
   hasFalling(){
@@ -47,30 +47,40 @@ export class Board {
   }
   
   tick(){
-    canfall = true
-    for (let y = 0; y < this.tetromino.size; y++){
-      for (let x = 0; x < this.tetromino.size; x++){
-        if (this.tetromino[y][x]){
-          canfall = canfall && this.canFall(this.tetrominox+x, this.tetrominoy+y);
+    if (this.hasFalling()){
+      let canfall = true
+      for (let y = 0; y < this.tetromino.size; y++){
+        for (let x = 0; x < this.tetromino.size; x++){
+          if (this.tetromino.matrix[y][x]){
+            canfall = canfall && this.canFall(this.tetrominox+x, this.tetrominoy+y);
+          }
         }
       }
-    }
-    if (canfall){
-      this.tetrominoy += 1;
-    }
+      if (canfall){
+        this.tetrominoy += 1;
+      } else {
+        for (let y = 0; y < this.tetromino.size; y++){
+          for (let x = 0; x < this.tetromino.size; x++){
+            if (this.tetromino.matrix[y][x]){
+              this.boardState[this.tetrominoy+y][this.tetrominox+x] = this.tetromino.color;
+            }
+          }
+        }
+        this.tetromino = undefined
+      }
+    }    
   }
 
   toString() {
     let res = "";
     for (let r = 0; r < this.height; r++){
       for (let c = 0; c < this.width; c++){
-          console.log(c,r);
-        if (r >= this.tetrominoy && r < this.tetrominoy + this.tetromino.size 
+        if (this.hasFalling() && r >= this.tetrominoy && r < this.tetrominoy + this.tetromino.size 
         && c >= this.tetrominox && c < this.tetrominox + this.tetromino.size 
         && this.tetromino.matrix[r-this.tetrominoy][c-this.tetrominox]){
           res += this.tetromino.color;
         } else {
-          res += ".";
+          res += this.boardState[r][c];
         }
       }
       res += "\n";
