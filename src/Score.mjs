6c540fecaa5c7454175c6cec6738a6a10106a501
Juho Taipale <krooh@hotmail.com>
2023-01-19 15:19:04 +0200

    function calculate score according to Original Nintendo Scoring System

--- src/Score.mjs
+++ src/Score.mjs
@@ -7,5 +7,12 @@ export class Score {
     this.score = 0;
   }
   
-    
+  calculateScore(rows){
+    switch(rows){
+      case 1: return 40 * (this.level + 1)
+      case 2: return 100 * (this.level + 1)
+      case 3: return 300 * (this.level + 1) 	
+      default: return 1200 * (this.level + 1)
+    }
+
 }
