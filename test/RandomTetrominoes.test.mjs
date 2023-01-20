396693cf9ca8b106d252644077ca6260356a4b3b
Juho Taipale <krooh@hotmail.com>
2023-01-20 06:52:03 +0200

    tests for dropping random tetrominoes

---
+++ test/RandomTetrominoes.test.mjs
@@ -0,0 +1,36 @@
+import { expect } from "chai";
+import { Tetromino } from "../src/Tetromino.mjs";
+import { MyShuffleBag } from "../src/MyShuffleBag.mjs";
+
+describe("Drop Random Tetrominoes", () => {
+  let shufflebag;
+  let tetrominoes;
+  beforeEach(() => {
+    tetrominoes = 
+    [...Array(10).fill(Tetromino.T_SHAPE),
+    ...Array(10).fill(Tetromino.I_SHAPE),
+    ...Array(10).fill(Tetromino.O_SHAPE),
+    ...Array(10).fill(Tetromino.S_SHAPE),
+    ...Array(10).fill(Tetromino.Z_SHAPE),
+    ...Array(10).fill(Tetromino.L_SHAPE),
+    ...Array(10).fill(Tetromino.M_SHAPE)];
+    shufflebag = new MyShuffleBag(tetrominoes);
+  });
+
+  it("check that all tetrominoes get dropped", () => {
+    let tet = []
+    for (let i=0; i < tetrominoes.length-9; i++){
+      tet.push(shufflebag.getRandomObject());
+    }
+    for (let i = 0; i < tetrominoes.length; i++){
+      expect(tet).to.include(tetrominoes[i]);
+    }
+  });
+
+  it("check that two shuffle bags are different. This test might fail if you are super unlucky.", () => {
+      const one = [...shufflebag.bag];
+      shufflebag.shuffle();
+      const two = [...shufflebag.bag];
+      expect([...shufflebag.bag]).to.not.eql([...one]);
+  });
+});
